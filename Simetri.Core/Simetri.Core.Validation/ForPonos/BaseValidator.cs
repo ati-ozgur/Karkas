d20fd10b5f773d7374b8ad9857437de9dc16ddf4
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2007-12-15 16:52:31 +0000

    DebuggerDisplay ekledim.
    git-svn-id: https://karkas.googlecode.com/svn/trunk@255 ac15386d-a63e-0410-a15c-c17449491352

--- Simetri.Core/Simetri.Core.Validation/ForPonos/BaseValidator.cs
+++ Simetri.Core/Simetri.Core.Validation/ForPonos/BaseValidator.cs
@@ -2,90 +2,92 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace Simetri.Core.Validation.ForPonos
 {
-        [Serializable]
-        public abstract class BaseValidator
+    [Serializable]
+    [DebuggerDisplay("PropertyName={PropertyName},ErrorMessage={ErrorMessage}")]
+    public abstract class BaseValidator
+    {
+        string propertyName;
+
+        public string PropertyName
         {
-            string propertyName;
-
-            public string PropertyName
-            {
-                get { return propertyName; }
-                set { propertyName = value; }
-            }
-            public BaseValidator(object uzerindeCalisilacakNesne, string pPropertyName)
-            {
-                this.propertyName = pPropertyName;
-                Type t = uzerindeCalisilacakNesne.GetType();
-                property = t.GetProperty(propertyName);
-                ErrorMessage = BuildErrorMessage();
-            }
-            public BaseValidator(object uzerindeCalisilacakNesne, string pPropertyName,string pErrorMessage)
-            {
-                this.propertyName = pPropertyName;
-                Type t = uzerindeCalisilacakNesne.GetType();
-                property = t.GetProperty(propertyName);
-                ErrorMessage = pErrorMessage;
-            }
-
-
-            private String errorMessage;
-            private PropertyInfo property;
-
-            /// <summary>
-            /// Implementors should perform any initialization logic
-            /// </summary>
-            /// <param name="property">The target property</param>
-            public void Initialize(PropertyInfo property)
-            {
-                this.property = property;
-
-                if (errorMessage == null)
-                {
-                    errorMessage = BuildErrorMessage();
-                }
-            }
-
-            /// <summary>
-            /// The target property
-            /// </summary>
-            public PropertyInfo Property
-            {
-                get { return property; }
-            }
-
-            /// <summary>
-            /// The error message to be displayed if the validation fails
-            /// </summary>
-            public String ErrorMessage
-            {
-                get { return errorMessage; }
-                set { errorMessage = value; }
-            }
-
-            /// <summary>
-            /// Implementors should perform the actual validation upon
-            /// the property value
-            /// </summary>
-            /// <param name="instance"></param>
-            /// <returns><c>true</c> if the field is OK</returns>
-            public bool Perform(object instance)
-            {
-                return this.Perform(instance, Property.GetValue(instance, null));
-            }
-
-            /// <summary>
-            /// Implementors should perform the actual validation upon
-            /// the property value
-            /// </summary>
-            /// <param name="instance"></param>
-            /// <param name="fieldValue"></param>
-            /// <returns><c>true</c> if the field is OK</returns>
-            public abstract bool Perform(object instance, object fieldValue);
-
-            protected abstract string BuildErrorMessage();
+            get { return propertyName; }
+            set { propertyName = value; }
+        }
+        public BaseValidator(object uzerindeCalisilacakNesne, string pPropertyName)
+        {
+            this.propertyName = pPropertyName;
+            Type t = uzerindeCalisilacakNesne.GetType();
+            property = t.GetProperty(propertyName);
+            ErrorMessage = BuildErrorMessage();
+        }
+        public BaseValidator(object uzerindeCalisilacakNesne, string pPropertyName, string pErrorMessage)
+        {
+            this.propertyName = pPropertyName;
+            Type t = uzerindeCalisilacakNesne.GetType();
+            property = t.GetProperty(propertyName);
+            ErrorMessage = pErrorMessage;
         }
 
+
+        private String errorMessage;
+        private PropertyInfo property;
+
+        /// <summary>
+        /// Implementors should perform any initialization logic
+        /// </summary>
+        /// <param name="property">The target property</param>
+        public void Initialize(PropertyInfo property)
+        {
+            this.property = property;
+
+            if (errorMessage == null)
+            {
+                errorMessage = BuildErrorMessage();
+            }
+        }
+
+        /// <summary>
+        /// The target property
+        /// </summary>
+        public PropertyInfo Property
+        {
+            get { return property; }
+        }
+
+        /// <summary>
+        /// The error message to be displayed if the validation fails
+        /// </summary>
+        public String ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; }
+        }
+
+        /// <summary>
+        /// Implementors should perform the actual validation upon
+        /// the property value
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns><c>true</c> if the field is OK</returns>
+        public bool Perform(object instance)
+        {
+            return this.Perform(instance, Property.GetValue(instance, null));
+        }
+
+        /// <summary>
+        /// Implementors should perform the actual validation upon
+        /// the property value
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="fieldValue"></param>
+        /// <returns><c>true</c> if the field is OK</returns>
+        public abstract bool Perform(object instance, object fieldValue);
+
+        protected abstract string BuildErrorMessage();
     }
+
+}
