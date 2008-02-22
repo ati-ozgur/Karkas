501c862248a66f7978b78bbea85a002034d7db1a
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-02-22 13:07:27 +0000

    Email testleri eklendi. Daha önce hata olmuş.
    git-svn-id: https://karkas.googlecode.com/svn/trunk@279 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Simetri.Core/Simetri.Core.DataUtil.TestConsoleApp/Tests/OnaylamaTestleri/Email/EmailTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Simetri.Core.DataUtil.TestConsoleApp.TypeLibrary;
+using NUnit.Framework;
+using Simetri.Core.Validation.ForPonos;
+
+namespace Simetri.Core.DataUtil.TestConsoleApp.Tests.OnaylamaTestleri.Email
+{
+    [TestFixture]
+    public class EmailTest
+    {
+        // Methods
+        [Test]
+        public void TestGmail()
+        {
+            OrnekA a = this.testIcinOrnekAOlustur();
+            a.EmailDegisken = "Deneme";
+            Assert.IsFalse(a.Validate());
+            a.EmailDegisken = "deneme@gmail.com";
+            Assert.IsTrue(a.Validate());
+            a.EmailDegisken = "Deneme_Deneme.com";
+            Assert.IsFalse(a.Validate());
+        }
+
+        private OrnekA testIcinOrnekAOlustur()
+        {
+            OrnekA a = new OrnekA();
+            a.Validator.ValidatorList.Clear();
+            a.Validator.ValidatorList = new List<BaseValidator>();
+            a.Validator.ValidatorList.Add(new EmailValidator(a, "EmailDegisken"));
+            return a;
+        }
+    }
+
+ 
+
+}
---
+++ Simetri.Core/Simetri.Core.Validation/ForPonos/EmailValidator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Simetri.Core.Validation.ForPonos
+{
+    public class EmailValidator : RegExValidator
+    {
+        // Fields
+        protected const string REGEX_ONLY_CHARACTER = @"^[\w\.\-]+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]{1,})*(\.[a-zA-Z]{2,3}){1,2}$";
+
+        // Methods
+        public EmailValidator(object pUzerindeCalisilacakNesne, string pPropertyName)
+            : base(pUzerindeCalisilacakNesne, pPropertyName, @"^[\w\.\-]+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]{1,})*(\.[a-zA-Z]{2,3}){1,2}$", RegexOptions.None)
+        {
+        }
+
+        public EmailValidator(object pUzerindeCalisilacakNesne, string pPropertyName, string pErrorMessage)
+            : base(pUzerindeCalisilacakNesne, pPropertyName, @"^[\w\.\-]+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]{1,})*(\.[a-zA-Z]{2,3}){1,2}$", RegexOptions.None, pErrorMessage)
+        {
+        }
+
+        protected override string BuildErrorMessage()
+        {
+            return string.Format("{0} d\x00fczg\x00fcn bir email deðildir.", base.Property.Name);
+        }
+    }
+
+ 
+
+}
