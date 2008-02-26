53e5ef45e9f3a4740eba0e664ed2ac40169446f5
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-02-26 08:54:56 +0000

    Esit ve esitdegildir için test ekledim.
    git-svn-id: https://karkas.googlecode.com/svn/trunk@287 ac15386d-a63e-0410-a15c-c17449491352

--- Simetri.Core/Simetri.Core.DataUtil.TestConsoleApp/Tests/OnaylamaTestleri/KarsilastirmaTestleri.cs
+++ Simetri.Core/Simetri.Core.DataUtil.TestConsoleApp/Tests/OnaylamaTestleri/KarsilastirmaTestleri.cs
@@ -3,22 +3,64 @@ using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
 using Simetri.Core.DataUtil.TestConsoleApp.TypeLibrary;
+using Simetri.Core.Validation;
+using Simetri.Core.Validation.ForPonos;
 
 namespace Simetri.Core.DataUtil.TestConsoleApp.Tests.OnaylamaTestleri
 {
     [TestFixture]
     public class KarsilastirmaTestleri
     {
-        
+        private short Deger1 = 18;
+
         [Test]
         public void TestCastIntToShort()
         {
             OrnekA a = new OrnekA();
-            a.ShortDegisken = 25;
+            a.ShortDegisken = Deger1;
             Assert.IsTrue(a.Validate());
         }
-	
 
+        [Test]
+        public void TestEsittir()
+        {
+            OrnekA a = TestEsittirIcinOrneklustur();
+            a.ShortDegisken = Deger1;
+            Assert.IsTrue(a.Validate());
+            Deger1 = (short) (Deger1 + 1);
+            a.ShortDegisken =Deger1;
+            Assert.IsFalse(a.Validate());
+        }
+
+        [Test]
+        public void TestEsitDegildir()
+        {
+            OrnekA a = TestEsitDegildirIcinOrneklustur();
+            a.ShortDegisken = Deger1;
+            Assert.IsFalse(a.Validate());
+            Deger1 = (short)(Deger1 + 1);
+            a.ShortDegisken = Deger1;
+            Assert.IsTrue(a.Validate());
+        }
+
+        
+        private OrnekA TestEsitDegildirIcinOrneklustur()
+        {
+            OrnekA a = new OrnekA();
+            a.Validator.ValidatorList.Clear();
+            a.Validator.ValidatorList = new List<BaseValidator>();
+            a.Validator.ValidatorList.Add(new CompareValidator(a, "ShortDegisken", Deger1, CompareOperator.NotEqual));
+            return a;
+        }
+
+        private OrnekA TestEsittirIcinOrneklustur()
+        {
+            OrnekA a = new OrnekA();
+            a.Validator.ValidatorList.Clear();
+            a.Validator.ValidatorList = new List<BaseValidator>();
+            a.Validator.ValidatorList.Add(new CompareValidator(a, "ShortDegisken", Deger1, CompareOperator.Equal));
+            return a;
+        }
 
     }
 }
