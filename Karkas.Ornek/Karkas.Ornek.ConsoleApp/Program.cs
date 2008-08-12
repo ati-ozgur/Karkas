709d9dc09bc6a4f30c2cf4973a0a7956531919f1
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-08-12 12:06:57 +0000

    ornek console projesi
    git-svn-id: https://karkas.googlecode.com/svn/trunk@329 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Ornek/Karkas.Ornek.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Karkas.Ornek.TypeLibrary.Ornekler;
+using Karkas.Ornek.BsWrapper.Ornekler;
+
+namespace Karkas.Ornek.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m = new Musteri();
+            m.MusteriKey = Guid.NewGuid();
+            m.Adi = "Deneme";
+            m.Soyadi = "Deneme" + DateTime.Now.ToShortTimeString();
+
+            MusteriBsWrapper wrapper = new MusteriBsWrapper();
+            wrapper.Ekle(m);
+        }
+    }
+}
