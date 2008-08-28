c9190baa5f2e64a9973829f79f2e1890d58cb7d4
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-08-28 11:56:33 +0000

    Basit tesler
    git-svn-id: https://karkas.googlecode.com/svn/trunk@385 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Core/Karkas.Core.DataUtil.TestConsoleApp/Tests/DalTestleri/SiralamaTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Karkas.Core.DataUtil.TestConsoleApp.Tests.DalTestleri
+{
+    [TestFixture]
+    public class SiralamaTest1
+    {
+                // Methods
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+    }
+}
