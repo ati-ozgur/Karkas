c85c4a1cd968456d3d38cb81b0ce8d1d3896a51e
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-10-08 16:17:41 +0000

    isim degisikligi
    git-svn-id: https://karkas.googlecode.com/svn/trunk@448 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Core/Karkas.Core.DataUtil/Exceptions/KarkasVeriHatasi.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Karkas.Core.DataUtil.Exceptions
+{
+    [Serializable]
+    public class KarkasVeriHatasi : Exception
+    {
+        public KarkasVeriHatasi()
+            : base()
+        {
+
+        }
+        public KarkasVeriHatasi(SerializationInfo pInfo, StreamingContext pContext)
+            : base(pInfo, pContext)
+        {
+
+        }
+        public KarkasVeriHatasi(string pMessage)
+            : base(pMessage)
+        {
+
+        }
+        public KarkasVeriHatasi(string pMessage, Exception pInnerException)
+            : base(pMessage, pInnerException)
+        {
+
+        }
+
+    }
+}
