7836eb3f9e2dc87b00fe95ba1837ae7c5def4513
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-11-04 18:28:38 +0000

    Ornek enumlar
    git-svn-id: https://karkas.googlecode.com/svn/trunk@492 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Ornek/TypeLibrary/Karkas.Ornek.TypeLibrary/Enums/TtOrnekler/OrnekTur.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karkas.Ornek.TypeLibrary.Enums.TtOrnekler
+{
+	public partial class OrnekTurEnum
+    {
+		public const byte DenemeSpaceTuru = 1;
+		public const byte Dash = 2;
+		public const byte Yok = 3;
+		public const byte Space = 4;
+    }                   
+
+
+}
