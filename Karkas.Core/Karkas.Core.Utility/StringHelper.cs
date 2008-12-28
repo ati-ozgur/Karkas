05f2f7c777c8a83cb1441b7e71f61c7734d87cde
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-12-28 12:34:08 +0000

    StringHelper.cs eklendi.
    git-svn-id: https://karkas.googlecode.com/svn/trunk@559 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Core/Karkas.Core.Utility/StringHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Karkas.Core.Utility
+{
+    public static  class StringHelper
+    {
+        public static string StringToBase64Encode(this string str)
+        {
+            byte[] arr = StringToByteArray(str);
+            return Convert.ToBase64String(arr);
+        }
+        public static string StringToBase64Decode(this string str)
+        {
+            byte[] arr = Convert.FromBase64String(str);
+            return ByteArrayToString(arr);
+        }
+        public static string ByteArrayToString(this byte[] arr)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            return encoding.GetString(arr);
+        }
+        public static byte[] StringToByteArray(this string str)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            return encoding.GetBytes(str);
+        }
+    }
+}
