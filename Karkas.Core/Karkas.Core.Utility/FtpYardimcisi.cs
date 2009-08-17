3f517d1cb3a911cb6b8cfd81de38f871b578f1db
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2009-08-17 06:23:50 +0000

    FtpYardimcisi.cs ornek olarak eklendi.
    git-svn-id: https://karkas.googlecode.com/svn/trunk@644 ac15386d-a63e-0410-a15c-c17449491352

---
+++ Karkas.Core/Karkas.Core.Utility/FtpYardimcisi.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Karkas.Core.Utility
+{
+    public class FtpYardimcisi
+    {
+        private string hostName;
+
+        public string HostName
+        {
+            get { return hostName; }
+            set { hostName = value; }
+        }
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public FtpYardimcisi()
+        {
+
+        }
+
+
+        public FtpYardimcisi(string pHostName, string pUserName, string pPassword)
+        {
+            this.hostName = pHostName;
+            this.userName = pUserName;
+            this.password = pPassword;
+
+        }
+
+
+        private FtpWebRequest ftpWebRequest;
+
+        public FtpWebRequest getFtpWebRequest(string pRemoteFile)
+        {
+            ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(hostName + pRemoteFile);
+            ftpWebRequest.Credentials = new NetworkCredential(UserName, Password);
+            ftpWebRequest.KeepAlive = false;
+            ftpWebRequest.UseBinary = true;
+            return ftpWebRequest;
+        }
+
+
+        public void UpLoadWithFullFileName(string pRemoteLocation, string pLocalFile)
+        {
+            FtpWebRequest ftp = getFtpWebRequest(pRemoteLocation);
+            ftp.Method = WebRequestMethods.Ftp.UploadFile;
+            using (FileStream fs = File.OpenRead(pLocalFile))
+            {
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                using (Stream ftpstream = ftp.GetRequestStream())
+                {
+                    ftpstream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+
+
+        public void UpLoadWithDirectoryName(string pDirectoryName, string pLocalFileName)
+        {
+            UpLoadWithFullFileName(pDirectoryName + "//" + pLocalFileName, pLocalFileName);
+        }
+    }
+}
