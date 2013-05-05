8ce9a03fa80e2da3b00fa25c5e40bc5585e57b0e
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-06 00:12:47 +0300

    referans azaltma

--- Karkas.Core/Karkas.Core.DataUtil/PagingHelper.cs
+++ Karkas.Core/Karkas.Core.DataUtil/PagingHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using System.Text.RegularExpressions;
-using Karkas.Core.Utility;
 using Karkas.Extensions;
 using System.Data.Common;
 
