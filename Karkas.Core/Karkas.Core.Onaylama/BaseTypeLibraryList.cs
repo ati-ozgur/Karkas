5ba0eedb6a8a7360f4621bf8bcae6dfe05b044d4
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 01:59:35 +0300

    rename folder

---
+++ Karkas.Core/Karkas.Core.Onaylama/BaseTypeLibraryList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Karkas.Core.TypeLibrary
+{
+    public class BaseTypeLibraryList : List<BaseTypeLibrary>
+    {
+
+    }
+}
+
---
+++ Karkas.Core/Karkas.Core.Onaylama/ForPonos/MinDegerOnaylayici.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karkas.Core.Onaylama.ForPonos
+{
+    public class MinDegerOnaylayici : KarsilastirmaOnaylayici
+    {
+        private const KarsilastirmaOperatoru KARSILASTIRMA_OP = KarsilastirmaOperatoru.BuyukEsittir;
+
+        public MinDegerOnaylayici(object pUzerindeCalisilacakNesne
+                        , string pPropertyName,
+                        IComparable pKarsilastirilacakDeger)
+                        : base( pUzerindeCalisilacakNesne, pPropertyName, pKarsilastirilacakDeger, KARSILASTIRMA_OP)
+        {
+
+        }
+        public MinDegerOnaylayici(object pUzerindeCalisilacakNesne, string pPropertyName,
+            IComparable pKarsilastirilacakDeger, string pErrorMessage)
+            : base(pUzerindeCalisilacakNesne, pPropertyName, pKarsilastirilacakDeger, KARSILASTIRMA_OP, pErrorMessage)
+        {
+
+        }
+
+
+    }
+}
+
