8681f0d95c9d22443ecb310f43406473cb9b1675
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 01:55:08 +0300

    bunu daha sonra otomatize etmek lazım ama nuget package yapıyor su an

--- Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
+++ Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
@@ -1,8 +1,8 @@
-remove-item lib -recurse
-mkdir lib
+remove-item Karkas.Core.Onaylama/lib -recurse
+mkdir Karkas.Core.Onaylama/lib
 #mkdir tools
 #mkdir content
 msbuild   "..\Karkas.Core.2010.sln"
-copy ../Karkas.Core.Validation/bin/Debug/Karkas.Core.Onaylama.dll lib
-../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  Karkas.Core.Onaylama.nuspec
+copy ../Karkas.Core.Validation/bin/Debug/Karkas.Core.Onaylama.dll Karkas.Core.Onaylama/lib
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  Karkas.Core.Onaylama/Karkas.Core.Onaylama.nuspec
 
