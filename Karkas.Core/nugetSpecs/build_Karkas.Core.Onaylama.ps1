460c46c5c7df254df6050883dc5cba57516e93f6
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 01:57:13 +0300

    refactor script

--- Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
+++ Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
@@ -1,8 +1,9 @@
-remove-item Karkas.Core.Onaylama/lib -recurse
-mkdir Karkas.Core.Onaylama/lib
+$projectName = "Karkas.Core.Onaylama"
+remove-item $projectName/lib -recurse
+mkdir $projectName/lib
 #mkdir tools
 #mkdir content
 msbuild   "..\Karkas.Core.2010.sln"
-copy ../Karkas.Core.Validation/bin/Debug/Karkas.Core.Onaylama.dll Karkas.Core.Onaylama/lib
-../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  Karkas.Core.Onaylama/Karkas.Core.Onaylama.nuspec
+copy ../Karkas.Core.Validation/bin/Debug/$projectName.dll $projectName/lib
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec
 
