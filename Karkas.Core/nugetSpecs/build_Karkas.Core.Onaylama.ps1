df7eaeb7d0c1a6dbe033dfa73e8c8a91dd15cad0
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 02:03:08 +0300

    script refactor. version bilgisini bir dosyadan alıyor

--- Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
+++ Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
@@ -1,9 +1,10 @@
 $projectName = "Karkas.Core.Onaylama"
+$version = Get-Content version.txt
 remove-item $projectName/lib -recurse
 mkdir $projectName/lib
 #mkdir tools
 #mkdir content
 msbuild   "..\Karkas.Core.2010.sln"
 copy ../$projectName/bin/Debug/$projectName.dll $projectName/lib
-../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec -Version $version
 
