8e625f6b46392776e4ff7ccd4063904d385e04b2
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 02:12:16 +0300

    refactor script

--- Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
+++ Karkas.Core/nugetSpecs/build_Karkas.Core.Onaylama.ps1
@@ -1,5 +1,7 @@
 $projectName = "Karkas.Core.Onaylama"
 $version = Get-Content version.txt
+$nugetCacheFolder = "$Home/AppData/Local/NuGet/Cache"
+$nugetPackageName = "$projectName.$version.nupkg"
 remove-item $projectName/lib -recurse
 mkdir $projectName/lib
 #mkdir tools
@@ -7,4 +9,5 @@ mkdir $projectName/lib
 msbuild   "..\Karkas.Core.2010.sln"
 copy ../$projectName/bin/Debug/$projectName.dll $projectName/lib
 ../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec -Version $version
-
+move $nugetPackageName  $nugetCacheFolder -Force
+write-host "moved $nugetPackageName to $nugetCacheFolder "
