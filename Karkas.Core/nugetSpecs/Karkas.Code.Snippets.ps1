724dec1bc74ff8756840ab974a98ef3553d27ee3
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 22:16:48 +0300

    Code.Snippets nuget build script hazır

---
+++ Karkas.Core/nugetSpecs/Karkas.Code.Snippets.ps1
@@ -0,0 +1,11 @@
+$projectName = "Karkas.Code.Snippets"
+$version = Get-Content "$projectName.version.txt"
+write-host "version: " + $version
+$nugetCacheFolder = "$Home/AppData/Local/NuGet/Cache"
+$nugetPackageName = "$projectName.$version.nupkg"
+remove-item $projectName/lib -recurse
+#mkdir tools
+#mkdir content
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec -Version $version
+move $nugetPackageName  $nugetCacheFolder -Force
+write-host "moved $nugetPackageName to $nugetCacheFolder "
