be4c6f67043fe4586764a22a1b9ccd24b42e3383
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-11 11:25:18 +0300

    Karkas.Core.ExtensionsMethods nuget package

---
+++ Karkas.Core/nugetSpecs/build_Karkas.Core.ExtensionsMethods.ps1
@@ -0,0 +1,13 @@
+$projectName = "Karkas.Core.ExtensionsMethods"
+$version = Get-Content version.txt
+$nugetCacheFolder = "$Home/AppData/Local/NuGet/Cache"
+$nugetPackageName = "$projectName.$version.nupkg"
+remove-item $projectName/lib -recurse
+mkdir $projectName/lib
+#mkdir tools
+#mkdir content
+msbuild   "..\Karkas.Core.2010.sln"
+copy ../$projectName/bin/Debug/$projectName.dll $projectName/lib
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec -Version $version
+move $nugetPackageName  $nugetCacheFolder -Force
+write-host "moved $nugetPackageName to $nugetCacheFolder "
