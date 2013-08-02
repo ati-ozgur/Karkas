fdee3b69aaf0a0d42b935367368ee072f1f96328
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-03 00:12:57 +0300

    nuget publish code added

---
+++ Karkas.Core/nugetSpecs/publish_nuget_ALL.ps1
@@ -0,0 +1,8 @@
+# TODO su an herhangi bir exception handling yok. 
+# ../packages/NuGet.CommandLine.2.5.0/tools/NuGet setApiKey XXX
+
+.\build_nuget_ALL.ps1
+.\publish_nuget_Karkas.Code.Snippets.ps1
+.\publish_nuget_Karkas.Core.Onaylama.ps1
+.\publish_nuget_Karkas.Core.ExtensionsMethods.ps1
+.\publish_nuget_Karkas.Core.DataUtil.ps1
---
+++ Karkas.Core/nugetSpecs/publish_nuget_Karkas.Core.DataUtil.ps1
@@ -0,0 +1,8 @@
+$projectName = "Karkas.Core.DataUtil"
+$version = Get-Content karkas.core.version.txt
+$nugetCacheFolder = "$Home/AppData/Local/NuGet/Cache"
+$nugetPackageName = "$projectName.$version.nupkg"
+
+../packages/NuGet.CommandLine.2.5.0/tools/NuGet push   $nugetCacheFolder/$nugetPackageName 
+
+write-host "pushed $nugetPackageName"
