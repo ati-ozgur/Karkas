3d7221e7e6c2f978e412d2aff5eaa9be6ec30599
Atilla Ozgur <ati.ozgur@gmail.com>
2013-10-19 11:54:59 +0300

    Assembly info change

--- Karkas.Core/Karkas.Core.DataUtil/Properties/AssemblyInfo.cs
+++ Karkas.Core/Karkas.Core.DataUtil/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Karkas")]
 [assembly: AssemblyProduct("Karkas.Core.DataUtil")]
-[assembly: AssemblyCopyright("Copyright © Karkas 2007")]
+[assembly: AssemblyCopyright("Copyright © Karkas 2007-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.13")]
+[assembly: AssemblyFileVersion("1.0.13")]
 
