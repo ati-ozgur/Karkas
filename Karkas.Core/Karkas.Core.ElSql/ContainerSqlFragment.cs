530309679cf1f943173ba03017e25d6c50e28635
Atilla Ozgur <ati.ozgur@gmail.com>
2013-12-12 00:53:44 +0200

    ElSql port v2

---
+++ Karkas.Core/Karkas.Core.ElSql/ContainerSqlFragment.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkas.Core.ElSql
+{
+    /**
+     * Representation of a list of child units.
+     */
+    public class ContainerSqlFragment : SqlFragment
+    {
+
+        /**
+         * The fragments.
+         */
+        private List<SqlFragment> _fragments = new List<SqlFragment>();
+
+        /**
+         * Creates an empty container.
+         */
+        public ContainerSqlFragment()
+        {
+        }
+
+        //-------------------------------------------------------------------------
+        /**
+         * Adds a fragment to the list in the container.
+         * 
+         * @param childFragment  the child fragment, not null
+         */
+        public void addFragment(SqlFragment childFragment)
+        {
+            _fragments.Add(childFragment);
+        }
+
+        /**
+         * Gets the list of fragments.
+         * 
+         * @return the unmodifiable list of fragments, not null
+         */
+        public IList<SqlFragment> getFragments()
+        {
+            return _fragments.AsReadOnly();
+        }
+
+        //-------------------------------------------------------------------------
+
+        public override void toSQL(StringBuilder buf, ElSqlBundle bundle, DbParameter[] paramSource, int loopIndex)
+        {
+            foreach (SqlFragment fragment in _fragments)
+            {
+                fragment.toSQL(buf, bundle, paramSource, loopIndex);
+            }
+        }
+
+        //-------------------------------------------------------------------------
+        public String toString()
+        {
+            return getFragments().ToString();
+        }
+
+    }
+}
---
+++ Karkas.Core/Karkas.Core.ElSql/NameSqlFragment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkas.Core.ElSql
+{
+    public class NameSqlFragment : ContainerSqlFragment {
+
+  private String _name;
+
+  public NameSqlFragment(String name) {
+    if (name == null) {
+      throw new  ArgumentException("Name must be specified");
+    }
+    _name = name;
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Gets the name of the fragment.
+   * 
+   * @return the name, not null
+   */
+  String getName() {
+    return _name;
+  }
+
+  //-------------------------------------------------------------------------
+  public override String ToString() {
+    return GetType().Name  + ":" + _name + " " + getFragments();
+  }
+
+}
+}
