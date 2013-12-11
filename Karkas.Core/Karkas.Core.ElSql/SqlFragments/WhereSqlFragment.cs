695e094ce93fff686c8d4db6d48314d2951825cf
Atilla Ozgur <ati.ozgur@gmail.com>
2013-12-12 01:28:37 +0200

    ElSql port v8

---
+++ Karkas.Core/Karkas.Core.ElSql/SqlFragments/WhereSqlFragment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkas.Core.ElSql.SqlFragments
+{
+/**
+ * Representation of WHERE.
+ * <p>
+ * This outputs a WHERE clause if at least one child was output.
+ */
+public class WhereSqlFragment : ContainerSqlFragment {
+
+  /**
+   * Creates an instance.
+   */
+  WhereSqlFragment() {
+  }
+
+  //-------------------------------------------------------------------------
+  
+  public override void toSQL(StringBuilder buf, ElSqlBundle bundle, SqlParameterSource paramSource, int loopIndex) {
+    int oldLen = buf.Length;
+    buf.Append("WHERE ");
+    int newLen = buf.Length;
+    base.toSQL(buf, bundle, paramSource, loopIndex);
+    if (buf.Length == newLen) {
+      buf.Length = oldLen;
+    }
+  }
+
+  //-------------------------------------------------------------------------
+  public String ToString() {
+      return GetType().Name + " " + getFragments();
+  }
+
+}
+
+}
