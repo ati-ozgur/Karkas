a63587c0a8c5c1f8ccbc9fc9a80eae9c38368b1b
Atilla Ozgur <ati.ozgur@gmail.com>
2021-12-15 14:49:18 +0000

    moved to new

---
+++ New/Karkas.Core/Karkas.Core.DataUtil/TransactionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using Karkas.Core.TypeLibrary;
+
+namespace Karkas.Core.DataUtil
+{
+}
+
