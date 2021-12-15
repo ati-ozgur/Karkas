c8f5afe166f016da2f33a832db705b16c93237e0
Atilla Ozgur <ati.ozgur@gmail.com>
2021-12-15 18:00:04 +0000

    compiling project

--- New/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/FormAbout.cs
+++ New/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/FormAbout.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.Deployment.Application;
 
 namespace Karkas.CodeGeneration.WinApp
 {
@@ -19,12 +18,7 @@ namespace Karkas.CodeGeneration.WinApp
 
         private void FormAbout_Load(object sender, EventArgs e)
         {
-            if (ApplicationDeployment.IsNetworkDeployed)
-            {
-                var version = ApplicationDeployment.CurrentDeployment.CurrentVersion;
-                string strVersion = string.Format("Karkas Code Generation Version : v{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
-                labelVersion.Text = strVersion;
-            }
+            labelVersion.Text = "2.0";
         }
     }
 }
