1e1356783bc08178bb9b7f460a91410c0beac577
Atilla Oezguer <ati.ozgur@gmail.com>
2022-11-13 22:23:52 +0100

    upper and lower keywords added

--- Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Generators/OracleDalGenerator.cs
+++ Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Generators/OracleDalGenerator.cs
@@ -117,7 +117,33 @@ namespace Karkas.CodeGeneration.Oracle.Generators
             AtEndCurlyBraceletDescreaseTab(output);
         }
 
-        private static string[] reservedKeyWordsArray = { "ACCESS", "ADD", "ALL", "ALTER", "AND", "ANY", "AS", "ASC", "AUDIT", "BETWEEN", "BY", "CHAR", "CHECK", "CLUSTER", "COLUMN", "COLUMN_VALUE", "COMMENT", "COMPRESS", "CONNECT", "CREATE", "CURRENT", "DATE", "DECIMAL", "DEFAULT", "DELETE", "DESC", "DISTINCT", "DROP", "ELSE", "EXCLUSIVE", "EXISTS", "FILE", "FLOAT", "FOR", "FROM", "GRANT", "GROUP", "HAVING", "IDENTIFIED", "IMMEDIATE", "IN", "INCREMENT", "INDEX", "INITIAL", "INSERT", "INTEGER", "INTERSECT", "INTO", "IS", "LEVEL", "LIKE", "LOCK", "LONG", "MAXEXTENTS", "MINUS", "MLSLABEL", "MODE", "MODIFY", "NESTED_TABLE_ID", "NOAUDIT", "NOCOMPRESS", "NOT", "NOWAIT", "NULL", "NUMBER", "OF", "OFFLINE", "ON", "ONLINE", "OPTION", "OR", "ORDER", "PCTFREE", "PRIOR", "PUBLIC", "RAW", "RENAME", "RESOURCE", "REVOKE", "ROW", "ROWID ", "ROWNUM", "ROWS", "SELECT", "SESSION", "SET", "SHARE", "SIZE", "SMALLINT", "START", "SUCCESSFUL", "SYNONYM", "SYSDATE", "TABLE", "THEN", "TO", "TRIGGER", "UID", "UNION", "UNIQUE", "UPDATE", "USER", "VALIDATE", "VALUES", "VARCHAR", "VARCHAR2", "VIEW", "WHENEVER", "WHERE", "WITH" };
+        private static string[] reservedKeyWordsArray = {  "ACCESS", "ADD", "ALL", "ALTER", "AND", 
+            "ANY", "AS", "ASC", "AUDIT", "BETWEEN", "BY", "CHAR", "CHECK", "CLUSTER", "COLUMN", 
+            "COLUMN_VALUE", "COMMENT", "COMPRESS", "CONNECT", "CREATE", "CURRENT", "DATE", 
+            "DECIMAL", "DEFAULT", "DELETE", "DESC", "DISTINCT", "DROP", "ELSE", "EXCLUSIVE", 
+            "EXISTS", "FILE", "FLOAT", "FOR", "FROM", "GRANT", "GROUP", "HAVING", "IDENTIFIED",
+            "IMMEDIATE", "IN", "INCREMENT", "INDEX", "INITIAL", "INSERT", "INTEGER", "INTERSECT", 
+            "INTO", "IS", "LEVEL", "LIKE", "LOCK", "LONG", "MAXEXTENTS", "MINUS", "MLSLABEL", 
+            "MODE", "MODIFY", "NESTED_TABLE_ID", "NOAUDIT", "NOCOMPRESS", "NOT", "NOWAIT",
+            "NULL", "NUMBER", "OF", "OFFLINE", "ON", "ONLINE", "OPTION", "OR", "ORDER", 
+            "PCTFREE", "PRIOR", "PUBLIC", "RAW", "RENAME", "RESOURCE", "REVOKE", "ROW", 
+            "ROWID ", "ROWNUM", "ROWS", "SELECT", "SESSION", "SET", "SHARE", "SIZE", "SMALLINT", 
+            "START", "SUCCESSFUL", "SYNONYM", "SYSDATE", "TABLE", "THEN", "TO", "TRIGGER", "UID", 
+            "UNION", "UNIQUE", "UPDATE", "USER", "VALIDATE", "VALUES", "VARCHAR", "VARCHAR2", 
+            "VIEW", "WHENEVER", "WHERE", "WITH",
+            "access", "add", "all", "alter", "and", "any", "as", "asc", "audit", "between", 
+            "by", "char", "check", "cluster", "column", "column_value", "comment", "compress", 
+            "connect", "create", "current", "date", "decimal", "default", "delete", "desc", 
+            "distinct", "drop", "else", "exclusive", "exists", "file", "float", "for", "from", 
+            "grant", "group", "having", "identified", "immediate", "in", "increment", "index",
+            "initial", "insert", "integer", "intersect", "into", "is", "level", "like", "lock",
+            "long", "maxextents", "minus", "mlslabel", "mode", "modify", "nested_table_id", 
+            "noaudit", "nocompress", "not", "nowait", "null", "number", "of", "offline", "on", 
+            "online", "option", "or", "order", "pctfree", "prior", "public", "raw", "rename", 
+            "resource", "revoke", "row", "rowid ", "rownum", "rows", "select", "session", "set", 
+            "share", "size", "smallint", "start", "successful", "synonym", "sysdate", "table", 
+            "then", "to", "trigger", "uid", "union", "unique", "update", "user", "validate", 
+            "values", "varchar", "varchar2", "view", "whenever", "where", "with"  };
 
         private static List<string> reservedKeyWordsList = new List<string>(reservedKeyWordsArray);
         public override List<string> GetReservedKeywords()
--- Karkas.CodeGeneration/Karkas.CodeGeneration.Sqlite/Generators/SqliteDalGenerator.cs
+++ Karkas.CodeGeneration/Karkas.CodeGeneration.Sqlite/Generators/SqliteDalGenerator.cs
@@ -122,7 +122,42 @@ namespace Karkas.CodeGeneration.Sqlite.Generators
         }
 
 
-        private static string[] reservedKeyWordsArray = {"ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ALWAYS", "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT", "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST", "CHECK", "COLLATE", "COLUMN", "COMMIT", "CONFLICT", "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DETACH", "DISTINCT", "DO", "DROP", "EACH", "ELSE", "END", "ESCAPE", "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL", "FILTER", "FIRST", "FOLLOWING", "FOR", "FOREIGN", "FROM", "FULL", "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT", "MATCH", "MATERIALIZED", "NATURAL", "NO", "NOT", "NOTHING", "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR", "ORDER", "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN", "PRAGMA", "PRECEDING", "PRIMARY", "QUERY", "RAISE", "RANGE", "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE", "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT", "ROLLBACK", "ROW", "ROWS", "SAVEPOINT", "SELECT", "SET", "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO", "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE", "UPDATE", "USING", "VACUUM", "VALUES", "VIEW", "VIRTUAL", "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT"};
+        private static string[] reservedKeyWordsArray = {"ABORT", "ACTION", "ADD", "AFTER", "ALL", 
+            "ALTER", "ALWAYS", "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT", "BEFORE",
+            "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST", "CHECK", "COLLATE", "COLUMN",
+            "COMMIT", "CONFLICT", "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", 
+            "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT", "DEFERRABLE",
+            "DEFERRED", "DELETE", "DESC", "DETACH", "DISTINCT", "DO", "DROP", "EACH",
+            "ELSE", "END", "ESCAPE", "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", 
+            "FAIL", "FILTER", "FIRST", "FOLLOWING", "FOR", "FOREIGN", "FROM", "FULL", 
+            "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", 
+            "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", 
+            "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST",
+            "LEFT", "LIKE", "LIMIT", "MATCH", "MATERIALIZED", "NATURAL", "NO", "NOT",
+            "NOTHING", "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR", "ORDER",
+            "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN", "PRAGMA", "PRECEDING", "PRIMARY",
+            "QUERY", "RAISE", "RANGE", "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE",
+            "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT", "ROLLBACK", "ROW", "ROWS",
+            "SAVEPOINT", "SELECT", "SET", "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO", 
+            "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE", "UPDATE", "USING", "VACUUM", 
+            "VALUES", "VIEW", "VIRTUAL", "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT",
+"abort",    "action", "add", "after", "all", "alter", "always", "analyze", "and", "as", "asc", 
+            "attach", "autoincrement", "before", "begin", "between", "by", "cascade", "case", 
+            "cast", "check", "collate", "column", "commit", "conflict", "constraint", "create", 
+            "cross", "current", "current_date", "current_time", "current_timestamp", "database",
+            "default", "deferrable", "deferred", "delete", "desc", "detach", "distinct", "do", 
+            "drop", "each", "else", "end", "escape", "except", "exclude", "exclusive", "exists", 
+            "explain", "fail", "filter", "first", "following", "for", "foreign", "from", "full",
+            "generated", "glob", "group", "groups", "having", "if", "ignore", "immediate", "in", 
+            "index", "indexed", "initially", "inner", "insert", "instead", "intersect", "into", "is", 
+            "isnull", "join", "key", "last", "left", "like", "limit", "match", "materialized",
+            "natural", "no", "not", "nothing", "notnull", "null", "nulls", "of", "offset", "on", 
+            "or", "order", "others", "outer", "over", "partition", "plan", "pragma", "preceding", 
+            "primary", "query", "raise", "range", "recursive", "references", "regexp", "reindex", 
+            "release", "rename", "replace", "restrict", "returning", "right", "rollback", "row",
+            "rows", "savepoint", "select", "set", "table", "temp", "temporary", "then", "ties",
+            "to", "transaction", "trigger", "unbounded", "union", "unique", "update", "using",
+            "vacuum", "values", "view", "virtual", "when", "where", "window", "with", "without"};
 
         private static List<string> reservedKeyWordsList = new List<string>(reservedKeyWordsArray);
         public override List<string> GetReservedKeywords()
