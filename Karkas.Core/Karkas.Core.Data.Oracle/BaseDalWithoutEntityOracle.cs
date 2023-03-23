7a6c756c8e2242ca8831954d1ccf7a403633f45d
Atilla Ozgur <ati.ozgur@gmail.com>
2023-03-23 14:17:06 +0300

    add BaseDalWithoutEntityOracle

---
+++ Karkas.Core/Karkas.Core.Data.Oracle/BaseDalWithoutEntityOracle.cs
@@ -0,0 +1,13 @@
+﻿using Karkas.Core.DataUtil.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karkas.Core.Data.Oracle
+{
+    public abstract class BaseDalWithoutEntityOracle : BaseDalWithoutEntity<AdoTemplateOracle, ParameterBuilderOracle>
+    {
+    }
+}
