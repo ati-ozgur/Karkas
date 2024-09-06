b0e604554f51158437249464f6c72224d2c74fbe
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-06 16:40:54 +0300

    Okay read config.json working

--- Karkas.CodeGeneration/Karkas.CodeGeneration.ConsoleApp/Program.cs
+++ Karkas.CodeGeneration/Karkas.CodeGeneration.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.IO;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+Console.WriteLine();
+
+string jsonFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}/config.json";
+
+string fileContents = File.ReadAllText(jsonFilePath);
+
+Console.WriteLine(fileContents);
