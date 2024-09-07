cb324c3567383be0e650830a891a68989565285f
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-07 17:39:14 +0300

    example program is added

---
+++ Karkas.Examples/TestCodes/SqliteProgramChinook.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Data.Sqlite;
+using Karkas.Examples.Chinook.Dal.ChinookSqlite;
+using System.Data.Common;
+using Karkas.Core.DataUtil;
+
+
+string dbConnectionString = "Data Source=Chinook.db;";
+string dbProviderName = "Microsoft.Data.Sqlite";
+string dbName = "ChinookSqlite";
+
+DbProviderFactories.RegisterFactory(dbProviderName, Microsoft.Data.Sqlite.SqliteFactory.Instance);
+
+ConnectionSingleton.Instance.AddConnection(dbName, dbProviderName, dbConnectionString);
+ConnectionSingleton.Instance.AddConnection("Main", dbProviderName, dbConnectionString);
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+AlbumDal dal = new AlbumDal();
+
+var liste = dal.QueryAll();
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item.Title);
+}
