21eb488f0fc9ecc9a095e2d5459714426b59b380
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-07 23:30:51 +0300

    update script for test

--- run_chinook_sqlite.bash
+++ run_chinook_sqlite.bash
@@ -5,6 +5,8 @@ dotnet new console
 dotnet add package Microsoft.Data.Sqlite
 dotnet add reference "../../../Karkas.Core/Karkas.Core.DataUtil/Karkas.Core.DataUtil.csproj"
 dotnet add reference "../../../Karkas.Core/Karkas.Core.Data.Sqlite/Karkas.Core.Data.Sqlite.csproj"
+cp ../../TestCodes/SqliteProgramChinook.cs Program.cs
+cp ../../Chinook/Chinook.db Chinook.db
 dotnet build
-
+dotnet run
 
