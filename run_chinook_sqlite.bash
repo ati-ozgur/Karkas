227677bab58fe454bf47655ae782d72fa7897827
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-08 21:03:24 +0300

    update bash script for early fail

--- run_chinook_sqlite.bash
+++ run_chinook_sqlite.bash
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# The -e option will cause a bash script to exit immediately when a command fails. 
+# The -u option causes the bash shell to treat unset variables as an error and exit immediately. 
+# The -x option causes bash to print each command before executing it. 
+# The -o pipefail option sets the exit code of a pipeline to 
+# that of the rightmost command to exit with a non-zero status.
+
+set -euxo pipefail
+
 rm -rf Karkas.Examples/GeneratedProjects/ChinookSqlite
 dotnet run --project Karkas.CodeGeneration/Karkas.CodeGeneration.ConsoleApp ChinookSqlite
 cd Karkas.Examples/GeneratedProjects/ChinookSqlite
