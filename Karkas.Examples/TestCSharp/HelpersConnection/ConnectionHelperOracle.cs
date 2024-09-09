08a319503878ac12c0c33c9849c4f97f03454e6b
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-10 01:12:01 +0300

    ConnectionHelper for Oracle fixed

--- Karkas.Examples/TestCSharp/HelpersConnection/ConnectionHelperOracle.cs
+++ Karkas.Examples/TestCSharp/HelpersConnection/ConnectionHelperOracle.cs
@@ -11,11 +11,11 @@ namespace Karkas.Examples
 
         public static void SetupDatabaseConnection()
         {
-            string dbConnectionString = "User Id=sa;Password=Karkas@Passw0rd;Persist Security Info=False;Initial Catalog=Chinook_AutoIncrement;Data Source=localhost;encrypt=False;";
+            string dbConnectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=xe)));User Id=chinook;Password=chinook;";
             string dbProviderName = "Oracle.ManagedDataAccess.Client";
-            string dbName = "ChinookSqlServerAutoIncrement";
+            string dbName = "ChinookOracle";
 
-            DbProviderFactories.RegisterFactory(dbProviderName, Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+            DbProviderFactories.RegisterFactory(dbProviderName, Oracle.ManagedDataAccess.Client.OracleClientFactory.Instance);
             ConnectionSingleton.Instance.AddConnection(dbName, dbProviderName, dbConnectionString);
             ConnectionSingleton.Instance.AddConnection("Main", dbProviderName, dbConnectionString);
 
