cc493929ece10a47df6b71f47765485dd76f174e
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-18 20:33:12 +0200

    BaseBsForIdentity v0

---
+++ Karkas.Core/Karkas.Core.DataUtil/BaseClasses/BaseBsForIdentity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Karkas.Core.DataUtil.BaseClasses;
+
+public abstract class BaseBsForIdentity<TYPE_LIBRARY_TYPE, DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER,PRIMARY_KEY> : BaseBs<TYPE_LIBRARY_TYPE,DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>
+    where TYPE_LIBRARY_TYPE : BaseTypeLibrary, new()
+    where ADOTEMPLATE_DB_TYPE : IAdoTemplate<IParameterBuilder>, new()
+    where DAL_TYPE : BaseDalForIdentity<TYPE_LIBRARY_TYPE, PRIMARY_KEY, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>, new()
+    where PARAMETER_BUILDER : IParameterBuilder, new()
+    where PRIMARY_KEY : struct
+{
+
+}
