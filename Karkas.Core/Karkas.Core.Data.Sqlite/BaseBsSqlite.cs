50e047670f53f36da1bcf7cb67ba0618b5a323fc
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-18 23:04:54 +0200

    new types compiles

---
+++ Karkas.Core/Karkas.Core.Data.Sqlite/BaseBsSqlite.cs
@@ -0,0 +1,35 @@
+using Karkas.Core.DataUtil;
+using Karkas.Core.DataUtil.BaseClasses;
+
+
+using Microsoft.Data.Sqlite;
+
+
+namespace Karkas.Core.Data.Sqlite
+{
+
+    public class BaseBsSqlite<TYPE_LIBRARY_TYPE,DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER> : BaseBs<TYPE_LIBRARY_TYPE, DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER> 
+      where TYPE_LIBRARY_TYPE : BaseTypeLibrary, new ()
+        where ADOTEMPLATE_DB_TYPE: IAdoTemplate<IParameterBuilder>, new ()
+        where DAL_TYPE : BaseDal<TYPE_LIBRARY_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>, new ()
+        where PARAMETER_BUILDER : IParameterBuilder, new ()
+
+    {
+
+
+        public new DIGER_DAL_TIPI GetDalInstance<DIGER_DAL_TIPI, DIGER_TYPE_LIBRARY_TIPI>()
+            where DIGER_TYPE_LIBRARY_TIPI : BaseTypeLibrary, new()
+            where DIGER_DAL_TIPI : BaseDal<DIGER_TYPE_LIBRARY_TIPI, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>, new()
+        {
+            DIGER_DAL_TIPI di = new DIGER_DAL_TIPI();
+            di.Connection = Connection;
+            di.IsInTransaction = IsInTransaction;
+            if (IsInTransaction)
+            {
+                di.AutomaticConnectionManagement = false;
+                di.CurrentTransaction = transaction;
+            }
+            return di;
+        }
+    }
+}
---
+++ Karkas.Core/Karkas.Core.Data.Sqlite/BaseBsSqliteForIdentity.cs
@@ -0,0 +1,36 @@
+using Karkas.Core.DataUtil;
+using Karkas.Core.DataUtil.BaseClasses;
+
+
+using Microsoft.Data.Sqlite;
+
+
+namespace Karkas.Core.Data.Sqlite
+{
+
+    public class BaseBsSqliteForIdentity<TYPE_LIBRARY_TYPE, DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER, PRIMARY_KEY>         
+        : BaseBsForIdentity<TYPE_LIBRARY_TYPE, DAL_TYPE, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER, PRIMARY_KEY>
+        where TYPE_LIBRARY_TYPE : BaseTypeLibrary, new()
+        where DAL_TYPE : BaseDalForIdentity<TYPE_LIBRARY_TYPE, PRIMARY_KEY, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>, new()
+        where ADOTEMPLATE_DB_TYPE : IAdoTemplate<IParameterBuilder>, new()
+        where PARAMETER_BUILDER : IParameterBuilder, new()
+        where PRIMARY_KEY : struct
+
+    {
+        public new DIGER_DAL_TIPI GetDalInstance<DIGER_DAL_TIPI, DIGER_TYPE_LIBRARY_TIPI>()
+            where DIGER_TYPE_LIBRARY_TIPI : BaseTypeLibrary, new()
+            where DIGER_DAL_TIPI : BaseDal<DIGER_TYPE_LIBRARY_TIPI, ADOTEMPLATE_DB_TYPE, PARAMETER_BUILDER>, new()
+        {
+            DIGER_DAL_TIPI di = new DIGER_DAL_TIPI();
+            di.Connection = Connection;
+            di.IsInTransaction = IsInTransaction;
+            if (IsInTransaction)
+            {
+                di.AutomaticConnectionManagement = false;
+                di.CurrentTransaction = transaction;
+            }
+            return di;
+        }
+
+    }
+}
