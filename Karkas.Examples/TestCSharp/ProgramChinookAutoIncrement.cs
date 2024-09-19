66113413abf0f12a928a7523a7d8cf664ea46b70
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-19 02:18:19 +0200

    add CRUD Customer tests

--- Karkas.Examples/TestCSharp/ProgramChinookAutoIncrement.cs
+++ Karkas.Examples/TestCSharp/ProgramChinookAutoIncrement.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Data.Common;
-
 using Karkas.Core.DataUtil;
-
 
 using Karkas.Examples;
 using Karkas.Examples.Chinook.Bs;
@@ -40,6 +38,99 @@ albumId == a.AlbumId
 else
 {
     throw new Exception("Insert for Identity does NOT work");
+}
+
+Customer c1 = new Customer();
+c1.FirstName = "Atilla";
+c1.Address = "MG Germany";
+c1.Company = "University";
+c1.Country = "Türkiye";
+c1.Email = "ati.ozgur@gmail.com";
+c1.Fax = "None";
+c1.LastName = "Özgür";
+c1.Phone = "123456";
+
+CustomerBs customerBs = new CustomerBs();
+
+var customerRowCountBefore = customerBs.TableRowCount;
+Console.WriteLine($"customerRowCountBefore: {customerRowCountBefore}");
+
+var newCustomerId = customerBs.Insert(c1);
+
+var customerRowCountAfter = customerBs.TableRowCount;
+Console.WriteLine($"customerRowCountAfter: {customerRowCountAfter}");
+
+
+if(customerRowCountAfter == customerRowCountBefore+1)
+{
+    Console.WriteLine("Insert to customer works");
+}
+else
+{
+    throw new Exception("Insert to customer DOES NOT works");
+}
+
+Customer fromDb = customerBs.QueryByCustomerId(newCustomerId);
+
+if(c1.CustomerId == fromDb.CustomerId
+&& c1.FirstName == fromDb.FirstName
+&& c1.Address == fromDb.Address
+&& c1.Company == fromDb.Company
+&& c1.Country == fromDb.Country
+&& c1.Email == fromDb.Email
+&& c1.Fax == fromDb.Fax
+&& c1.LastName == fromDb.LastName
+&& c1.Phone == fromDb.Phone
+)
+{
+    Console.WriteLine("Read from Customer table works");
+}
+else
+{
+    throw new Exception("Read from Customer table DOES NOT works");
+}
+
+fromDb.LastName = "Oezguer";
+
+customerBs.Update(fromDb);
+Customer fromDb2 = customerBs.QueryByCustomerId(newCustomerId);
+
+if (fromDb2.CustomerId == fromDb.CustomerId
+&& fromDb2.FirstName == fromDb.FirstName
+&& fromDb2.Address == fromDb.Address
+&& fromDb2.Company == fromDb.Company
+&& fromDb2.Country == fromDb.Country
+&& fromDb2.Email == fromDb.Email
+&& fromDb2.Fax == fromDb.Fax
+&& fromDb2.LastName == fromDb.LastName
+&& fromDb2.Phone == fromDb.Phone
+)
+{
+    Console.WriteLine("Update to Customer table works");
+}
+else
+{
+    throw new Exception("Update to Customer table DOES NOT works");
+}
+
+
+var customerRowCountBeforeDelete = customerBs.TableRowCount;
+
+Console.WriteLine($"customerRowCountBeforeDelete: {customerRowCountBeforeDelete}");
+
+customerBs.Delete(newCustomerId);
+
+var customerRowCountAfterDelete = customerBs.TableRowCount;
+
+Console.WriteLine($"customerRowCountAfterDelete: {customerRowCountAfterDelete}");
+
+if (customerRowCountAfterDelete == customerRowCountBeforeDelete - 1)
+{
+    Console.WriteLine("delete to customer works");
+}
+else
+{
+    throw new Exception("delete to customer DOES NOT works");
 }
 
 
