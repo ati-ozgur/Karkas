2f1179270b737ec4b70f4652b19e80e0d8416ff8
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-19 14:39:20 +0200

    update oracle scripts for quotes

--- Karkas.Examples/TestBash/chinook-oracle/0Chinook_Oracle_DDL_AUTOINCREMENT.sql
+++ Karkas.Examples/TestBash/chinook-oracle/0Chinook_Oracle_DDL_AUTOINCREMENT.sql
@@ -167,37 +167,37 @@ CREATE TABLE "Track"
    Create Foreign Keys
 ********************************************************************************/
 ALTER TABLE "Album" ADD CONSTRAINT FK_AlbumArtistId
-    FOREIGN KEY ("ArtistId") REFERENCES Artist ("ArtistId")  ;
+    FOREIGN KEY ("ArtistId") REFERENCES "Artist" ("ArtistId")  ;
 
 ALTER TABLE "Customer" ADD CONSTRAINT FK_CustomerSupportRepId
-    FOREIGN KEY ("SupportRepId") REFERENCES Employee ("EmployeeId")  ;
+    FOREIGN KEY ("SupportRepId") REFERENCES "Employee" ("EmployeeId")  ;
 
 ALTER TABLE "Employee" ADD CONSTRAINT FK_EmployeeReportsTo
-    FOREIGN KEY ("ReportsTo") REFERENCES Employee ("EmployeeId")  ;
+    FOREIGN KEY ("ReportsTo") REFERENCES "Employee" ("EmployeeId")  ;
 
 ALTER TABLE "Invoice" ADD CONSTRAINT FK_InvoiceCustomerId
-    FOREIGN KEY ("CustomerId") REFERENCES Customer ("CustomerId")  ;
+    FOREIGN KEY ("CustomerId") REFERENCES "Customer" ("CustomerId")  ;
 
 ALTER TABLE "InvoiceLine" ADD CONSTRAINT FK_InvoiceLineInvoiceId
-    FOREIGN KEY ("InvoiceId") REFERENCES Invoice ("InvoiceId")  ;
+    FOREIGN KEY ("InvoiceId") REFERENCES "Invoice" ("InvoiceId")  ;
 
 ALTER TABLE "InvoiceLine" ADD CONSTRAINT FK_InvoiceLineTrackId
-    FOREIGN KEY ("TrackId") REFERENCES Track ("TrackId")  ;
+    FOREIGN KEY ("TrackId") REFERENCES "Track" ("TrackId")  ;
 
 ALTER TABLE "PlaylistTrack" ADD CONSTRAINT FK_PlaylistTrackPlaylistId
-    FOREIGN KEY ("PlaylistId") REFERENCES Playlist ("PlaylistId")  ;
+    FOREIGN KEY ("PlaylistId") REFERENCES "Playlist" ("PlaylistId")  ;
 
 ALTER TABLE "PlaylistTrack" ADD CONSTRAINT FK_PlaylistTrackTrackId
-    FOREIGN KEY ("TrackId") REFERENCES Track ("TrackId")  ;
+    FOREIGN KEY ("TrackId") REFERENCES "Track" ("TrackId")  ;
 
 ALTER TABLE "Track" ADD CONSTRAINT FK_TrackAlbumId
-    FOREIGN KEY ("AlbumId") REFERENCES Album ("AlbumId")  ;
+    FOREIGN KEY ("AlbumId") REFERENCES "Album" ("AlbumId")  ;
 
 ALTER TABLE "Track" ADD CONSTRAINT FK_TrackGenreId
-    FOREIGN KEY ("GenreId") REFERENCES Genre ("GenreId")  ;
+    FOREIGN KEY ("GenreId") REFERENCES "Genre" ("GenreId")  ;
 
 ALTER TABLE "Track" ADD CONSTRAINT FK_TrackMediaTypeId
-    FOREIGN KEY ("MediaTypeId") REFERENCES MediaType ("MediaTypeId")  ;
+    FOREIGN KEY ("MediaTypeId") REFERENCES "MediaType" ("MediaTypeId")  ;
 
 
 commit;
