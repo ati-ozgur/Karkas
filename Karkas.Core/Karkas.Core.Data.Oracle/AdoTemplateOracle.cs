4d3db099dfac947ba67006486fc1266515bfaa0b
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-21 20:10:27 +0200

    exists works for oracle

--- Karkas.Core/Karkas.Core.Data.Oracle/AdoTemplateOracle.cs
+++ Karkas.Core/Karkas.Core.Data.Oracle/AdoTemplateOracle.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.Common;
+
 namespace Karkas.Core.Data.Oracle
 {
     public class AdoTemplateOracle : AdoTemplate<ParameterBuilderOracle>
@@ -17,5 +19,35 @@ namespace Karkas.Core.Data.Oracle
         public AdoTemplateOracle(string dbProviderName) : base(dbProviderName)
         {
         }
+
+        private string getSqlForExecuteAsExists(string pSql)
+        {
+            string sqlToExecute = string.Format(@"select count(*) 
+                                        from dual 
+                                        where exists({0})", pSql);
+            return sqlToExecute;
+        }
+        /// <summary>
+        /// Execute given sql statement inside IF EXISTS
+        /// If we have value, it return true otherwise false
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="prmListesi"></param>
+        /// <returns></returns>
+        public override bool ExecuteAsExists(String pSql, DbParameter[] pParamList)
+        {
+
+            object o = GetOneValue(getSqlForExecuteAsExists(pSql),pParamList);
+            int value = Convert.ToInt32(o);
+            return value > 0;
+        }
+        public override bool ExecuteAsExists(String pSql)
+        {
+            object o = GetOneValue(getSqlForExecuteAsExists(pSql));
+            int value = Convert.ToInt32(o);
+            return value > 0;
+        }
+
+
     }
 }
