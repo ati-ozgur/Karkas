c86fe55ef0813a66de06dd8928bcda8231dd817a
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-22 19:16:09 +0200

    Normal Crud test v1

--- Karkas.Examples/TestCSharp/Helpers/TestHelper.cs
+++ Karkas.Examples/TestCSharp/Helpers/TestHelper.cs
@@ -243,6 +243,41 @@ public class TestHelper
             Console.WriteLine("GetOneRow works");
         }
     }
+    public static void TestCrudNormalAlbum()
+    {
+        AlbumBs albumBs = new AlbumBs();
+
+
+
+        string albumTitle = "Deneme";
+
+        Album a = new Album();
+        a.AlbumId = 350;
+        a.Title = albumTitle;
+        a.ArtistId = 2;
+
+        long albumId = albumBs.Insert(a);
+
+        var albumList = albumBs.QueryUsingColumnName(Album.ColumnNames.Title, albumTitle);
+
+        Album b = albumList[albumList.Count - 1];
+        if (
+        albumId == a.AlbumId
+        && albumId == b.AlbumId
+        && a.AlbumId == b.AlbumId
+        && a.Title == b.Title
+        && a.ArtistId == b.ArtistId
+        )
+        {
+            Console.WriteLine("Insert for Identity works");
+        }
+        else
+        {
+            throw new Exception("Insert for Identity does NOT work");
+        }
+
+    }
+    
 
 }
 
