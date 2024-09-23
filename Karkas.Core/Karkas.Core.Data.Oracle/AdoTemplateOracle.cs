f89b489332102ca53e10f8d5aa7640978ea3af0c
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-23 15:40:11 +0200

    namespace file scoped

--- Karkas.Core/Karkas.Core.Data.Oracle/AdoTemplateOracle.cs
+++ Karkas.Core/Karkas.Core.Data.Oracle/AdoTemplateOracle.cs
@@ -1,5 +1,4 @@
-﻿using Karkas.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,69 +12,69 @@ using Karkas.Data;
 using Karkas.Data.Base;
 
 
-namespace Karkas.Data.Oracle
+namespace Karkas.Data.Oracle;
+
+public class AdoTemplateOracle : BaseAdoTemplate<ParameterBuilderOracle>
 {
-    public class AdoTemplateOracle : BaseAdoTemplate<ParameterBuilderOracle>
+    public AdoTemplateOracle()
     {
-        public AdoTemplateOracle()
-        {
-
-        }
-
-        public AdoTemplateOracle(string dbProviderName) : base(dbProviderName)
-        {
-        }
-
-        private string getSqlForExecuteAsExists(string pSql)
-        {
-            string sqlToExecute = string.Format(@"select count(*) 
-                                        from dual 
-                                        where exists({0})", pSql);
-            return sqlToExecute;
-        }
-        /// <summary>
-        /// Execute given sql statement inside IF EXISTS
-        /// If we have value, it return true otherwise false
-        /// </summary>
-        /// <param name="sql"></param>
-        /// <param name="prmListesi"></param>
-        /// <returns></returns>
-        public override bool ExecuteAsExists(String pSql, DbParameter[] pParamList)
-        {
-
-            object o = GetOneValue(getSqlForExecuteAsExists(pSql),pParamList);
-            int value = Convert.ToInt32(o);
-            return value > 0;
-        }
-        public override bool ExecuteAsExists(String pSql)
-        {
-            object o = GetOneValue(getSqlForExecuteAsExists(pSql));
-            int value = Convert.ToInt32(o);
-            return value > 0;
-        }
-
-        public override DbCommand getDatabaseCommand(DbConnection conn)
-        {
-            DbCommand command = conn.CreateCommand();
-            setBindByName(command);
-            return command;
-        }
-
-        // TODO look if I could move this one to OracleAdoTemplate
-        private void setBindByName(DbCommand command)
-        {
-            if (command.GetType().GetProperty("BindByName") != null)
-            {
-                command.GetType().GetProperty("BindByName").SetValue(command, true, null);
-            }
-        }
-
-
-        public override string ParameterSymbol
-        {
-            get { return ":"; }
-        }
-
 
     }
+
+    public AdoTemplateOracle(string dbProviderName) : base(dbProviderName)
+    {
+    }
+
+    private string getSqlForExecuteAsExists(string pSql)
+    {
+        string sqlToExecute = string.Format(@"select count(*) 
+                                    from dual 
+                                    where exists({0})", pSql);
+        return sqlToExecute;
+    }
+    /// <summary>
+    /// Execute given sql statement inside IF EXISTS
+    /// If we have value, it return true otherwise false
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <param name="prmListesi"></param>
+    /// <returns></returns>
+    public override bool ExecuteAsExists(String pSql, DbParameter[] pParamList)
+    {
+
+        object o = GetOneValue(getSqlForExecuteAsExists(pSql),pParamList);
+        int value = Convert.ToInt32(o);
+        return value > 0;
+    }
+    public override bool ExecuteAsExists(String pSql)
+    {
+        object o = GetOneValue(getSqlForExecuteAsExists(pSql));
+        int value = Convert.ToInt32(o);
+        return value > 0;
+    }
+
+    public override DbCommand getDatabaseCommand(DbConnection conn)
+    {
+        DbCommand command = conn.CreateCommand();
+        setBindByName(command);
+        return command;
+    }
+
+    // TODO look if I could move this one to OracleAdoTemplate
+    private void setBindByName(DbCommand command)
+    {
+        if (command.GetType().GetProperty("BindByName") != null)
+        {
+            command.GetType().GetProperty("BindByName").SetValue(command, true, null);
+        }
+    }
+
+
+    public override string ParameterSymbol
+    {
+        get { return ":"; }
+    }
+
+
 }
+
