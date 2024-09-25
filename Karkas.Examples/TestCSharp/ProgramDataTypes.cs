03043b73c642270a29ee3c93f5209c44d8303121
Atilla Ozgur <ati.ozgur@gmail.com>
2024-09-25 21:03:38 +0200

    test for OracleDataTypes v4

--- Karkas.Examples/TestCSharp/ProgramDataTypes.cs
+++ Karkas.Examples/TestCSharp/ProgramDataTypes.cs
@@ -1,4 +1,48 @@
 ﻿
-Console.WriteLine("Hello");
+
+ConnectionHelper.SetupDatabaseConnection();
+
+int pk = 1;
+
+Deneme d = new Deneme();
+
+
+
+d.CInt = 100;
+d.CInteger = 100;
+
+d.PkId = pk;
+
+
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < 1000; i++)
+{
+    sb.Append("1234567890");
+    sb.Append("1234567890");
+}
+
+string value = sb.ToString();
+Console.WriteLine("length:" + value.Length);
+
+Console.WriteLine(value);
+
+d.CClob = value;
+
+DenemeDal dal = new DenemeDal();
+
+dal.Insert(d);
+
+Deneme d2 = dal.QueryByPkId(pk);
+if(d2.CClob == d.CClob)
+{
+    Console.WriteLine("Clob insert works");
+}
+else
+{
+    throw new Exception("Clob insert DOES NOT work");    
+}
+
+
+
 
 
