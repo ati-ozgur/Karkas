c8d4fb666205bdc686e1f3f3437ff8def435367d
Atilla Ozgur <ati.ozgur@gmail.com>
2024-10-21 21:58:26 +0200

    version.txt added

--- deploy_nuget_packages.bash
+++ deploy_nuget_packages.bash
@@ -1,1 +1,6 @@
+source <(grep = version.txt)
+
+echo "KarkasDataVersion: $KarkasDataVersion"
+echo "KarkasCodeGenVersion: $KarkasCodeGenVersion"
+
 echo "KARKAS_NUGET_API_KEY: $KARKAS_NUGET_API_KEY"
