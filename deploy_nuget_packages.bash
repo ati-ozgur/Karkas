0c3a80ee99d25370094c2ead35f89efd80fd282f
Atilla Ozgur <ati.ozgur@gmail.com>
2024-10-21 22:07:08 +0200

    update script

--- deploy_nuget_packages.bash
+++ deploy_nuget_packages.bash
@@ -9,3 +9,7 @@ dotnet pack Karkas.Data/Karkas.Data/Karkas.Data.csproj /property:Version=$Karkas
 dotnet pack Karkas.Data/Karkas.Data.Sqlite/Karkas.Data.Sqlite.csproj /property:Version=$KarkasDataVersion
 dotnet pack Karkas.Data/Karkas.Data.Oracle/Karkas.Data.Oracle.csproj /property:Version=$KarkasDataVersion
 dotnet pack Karkas.Data/Karkas.Data.SqlServer/Karkas.Data.SqlServer.csproj /property:Version=$KarkasDataVersion
+dotnet nuget push Karkas.Data/Karkas.Data/bin/Release/Karkas.Data.$KarkasDataVersion.nupkg --api-key $KARKAS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push Karkas.Data/Karkas.Data.Sqlite/bin/Release/Karkas.Data.$KarkasDataVersion.nupkg --api-key $KARKAS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push Karkas.Data/Karkas.Data.Oracle/bin/Release/Karkas.Data.$KarkasDataVersion.nupkg --api-key $KARKAS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push Karkas.Data/Karkas.Data.SqlServer/bin/Release/Karkas.Data.$KarkasDataVersion.nupkg --api-key $KARKAS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
