76c1e6048b11e0d96fdd6b9c354240ae5add44e4
Atilla Ozgur <ati.ozgur@gmail.com>
2024-10-21 22:04:09 +0200

    update script

--- deploy_nuget_packages.bash
+++ deploy_nuget_packages.bash
@@ -4,3 +4,8 @@ echo "KarkasDataVersion: $KarkasDataVersion"
 echo "KarkasCodeGenVersion: $KarkasCodeGenVersion"
 
 echo "KARKAS_NUGET_API_KEY: $KARKAS_NUGET_API_KEY"
+
+dotnet pack Karkas.Data/Karkas.Data/Karkas.Data.csproj /property:Version=$KarkasDataVersion
+dotnet pack Karkas.Data/Karkas.Data.Sqlite/Karkas.Data.Sqlite.csproj /property:Version=$KarkasDataVersion
+dotnet pack Karkas.Data/Karkas.Data.Oracle/Karkas.Data.Oracle.csproj /property:Version=$KarkasDataVersion
+dotnet pack Karkas.Data/Karkas.Data.SqlServer/Karkas.Data.SqlServer.csproj /property:Version=$KarkasDataVersion
