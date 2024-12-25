5e324568756a55598ca62300a23654df39aca290
Atilla Ozgur <ati.ozgur@gmail.com>
2024-12-25 17:16:42 +0300

    Microsoft.SourceLink.GitHub added for debugging help

--- deploy_nuget_packages.bash
+++ deploy_nuget_packages.bash
@@ -5,12 +5,12 @@ echo "KarkasCodeGenVersion: $KarkasCodeGenVersion"
 
 echo "KARKAS_NUGET_API_KEY: $KARKAS_NUGET_API_KEY"
 
-dotnet pack Karkas.Data/Karkas.Data/Karkas.Data.csproj /property:Version=$KarkasDataVersion
-dotnet pack Karkas.Data/Karkas.Data.Sqlite/Karkas.Data.Sqlite.csproj /property:Version=$KarkasDataVersion
-dotnet pack Karkas.Data/Karkas.Data.Oracle/Karkas.Data.Oracle.csproj /property:Version=$KarkasDataVersion
-dotnet pack Karkas.Data/Karkas.Data.SqlServer/Karkas.Data.SqlServer.csproj /property:Version=$KarkasDataVersion
+dotnet pack Karkas.Data/Karkas.Data/Karkas.Data.csproj /property:Version=$KarkasDataVersion --include-symbols
+dotnet pack Karkas.Data/Karkas.Data.Sqlite/Karkas.Data.Sqlite.csproj /property:Version=$KarkasDataVersion --include-symbols
+dotnet pack Karkas.Data/Karkas.Data.Oracle/Karkas.Data.Oracle.csproj /property:Version=$KarkasDataVersion --include-symbols
+dotnet pack Karkas.Data/Karkas.Data.SqlServer/Karkas.Data.SqlServer.csproj /property:Version=$KarkasDataVersion --include-symbols
 
-dotnet pack Karkas.CodeGeneration/Karkas.CodeGeneration.ConsoleApp/Karkas.CodeGeneration.ConsoleApp.csproj /property:Version=$KarkasCodeGenVersion
+dotnet pack Karkas.CodeGeneration/Karkas.CodeGeneration.ConsoleApp/Karkas.CodeGeneration.ConsoleApp.csproj /property:Version=$KarkasCodeGenVersion --include-symbols
 echo "packing works"
 
 dotnet nuget push Karkas.Data/Karkas.Data/nupkg/Karkas.Data.$KarkasDataVersion.nupkg --api-key $KARKAS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json --skip-duplicate
