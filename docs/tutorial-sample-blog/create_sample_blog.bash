07f2721ced9ec1616b17982879f2f7087b3ee005
Atilla Ozgur <ati.ozgur@gmail.com>
2024-12-29 16:38:37 +0300

    update workflow

--- docs/tutorial-sample-blog/create_sample_blog.bash
+++ docs/tutorial-sample-blog/create_sample_blog.bash
@@ -3,10 +3,13 @@ karkas-codegen
 rm -rf sample-blog.sqlite
 rm -rf GeneratedProjects
 cat blog.sql | sqlite3 sample-blog.sqlite
-karkas-codegen -c sample -f karkas-config-sample-blog.json
-cd ./GeneratedProjects/sample
+karkas-codegen -c sample-blog -f karkas-config-sample-blog.json
+cd ./GeneratedProjects/sample-blog
 tree
 dotnet new console
 dotnet add package Karkas.Data
 dotnet add package Karkas.Data.Sqlite
 dotnet run
+rm -rf bin/
+rm -rf obj
+zip -r sample-blog.zip .
