3d8c84e8b21d3dfb8388aa8dad7494585b70f02d
Atilla Ozgur <ati.ozgur@gmail.com>
2024-12-30 15:50:09 +0300

    add to DataTypes code

--- Karkas.Examples/TestCSharp/Programs/ProgramDataTypesSqlServer.cs
+++ Karkas.Examples/TestCSharp/Programs/ProgramDataTypesSqlServer.cs
@@ -1,6 +1,19 @@
-﻿
+﻿ConnectionHelper.SetupDatabaseConnection();
 
-ConnectionHelper.SetupDatabaseConnection();
 
-int pk = 1;
 
+
+
+
+Deneme d1= new Deneme();
+d1.BitColumn = false;
+
+
+var pk = bs.Insert(d1);
+Console.WriteLine("pk" + pk);
+Console.WriteLine("Simple Insert works");
+
+DenemeBs bs = new DenemeBs();
+var l1 = bs.QueryAll();
+
+Console.WriteLine(l1);
