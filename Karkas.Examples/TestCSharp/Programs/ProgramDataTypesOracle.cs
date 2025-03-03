a2542412a5956459a2f6393da0e8f400a51f2b6e
Atilla Ozgur <ati.ozgur@gmail.com>
2025-03-03 20:01:10 +0100

    fix to Program.cs

--- Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
+++ Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
@@ -1,6 +1,26 @@
 ﻿
-
 ConnectionHelper.SetupDatabaseConnection();
 
-int pk = 1;
 
+DecimalTablesBs bs1 = new DecimalTablesBs();
+
+int rowCount = 10;
+var resultList = bs1.QueryAll(rowCount);
+
+
+AssertEquals(resultList.Count,rowCount);
+
+foreach (var row in resultList)
+{
+	Console.WriteLine(row.DecimalTablesKey);
+	Console.WriteLine(row.DecimalTablesKey);
+}
+
+
+static void AssertEquals(object p1, object p2, string message = "should be equal")
+{
+    if (p1 != p2)
+    {
+        throw new Exception($"Assertion error, {p1} != {p2} {message}");
+    }
+}
