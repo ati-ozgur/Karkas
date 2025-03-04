b939c64fd06801e50a2d4bab037b29c3789bcc38
Atilla Ozgur <ati.ozgur@gmail.com>
2025-03-04 17:23:43 +0100

    test v1 pass

--- Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Implementations/HelperOracleDataTypes.cs
+++ Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Implementations/HelperOracleDataTypes.cs
@@ -5,9 +5,69 @@ namespace Karkas.CodeGeneration.Oracle.Implementations;
 public class HelperOracleDataTypes
 {
 
-	public static string GetDotNetType(string dataTypeInDatabase)
+	public static string GetDotNetType(string pSqlTypeName)
 	{
-		return null;
+		pSqlTypeName = pSqlTypeName.ToLowerInvariant();
+		if (
+			pSqlTypeName.Equals("varchar") ||
+			pSqlTypeName.Equals("nvarchar") ||
+			pSqlTypeName.Equals("nvarchar2") ||
+			pSqlTypeName.Equals("varchar2") ||
+			pSqlTypeName.Equals("clob") ||
+			pSqlTypeName.Equals("nclob") ||
+			pSqlTypeName.Equals("char") ||
+			pSqlTypeName.Equals("nchar") ||
+			pSqlTypeName.Equals("ntext") ||
+			pSqlTypeName.Equals("xml") ||
+			pSqlTypeName.Equals("text") ||
+			pSqlTypeName.Equals("xmltype") ||
+			pSqlTypeName.Equals("xmltypeextra") ||
+			pSqlTypeName.Equals("xmltypepi")
+
+		)
+		{
+
+			return "string";
+		}
+
+		if (
+			pSqlTypeName.Equals("date")
+		)
+		{
+			return "DateTime";
+		}
+		if (
+
+			// TODO HERE
+			pSqlTypeName.Equals("number"))
+		{
+			return "decimal";
+		}
+		if (pSqlTypeName.Equals("float"))
+		{
+			return "double";
+		}
+		if (pSqlTypeName.Equals("real"))
+		{
+			return "float";
+		}
+		if (
+			pSqlTypeName.Equals("image") ||
+			pSqlTypeName.Equals("long") ||
+			pSqlTypeName.Equals("blob") ||
+			pSqlTypeName.Equals("binary") ||
+			pSqlTypeName.Equals("varbinary") ||
+			pSqlTypeName.Equals("timestamp")
+		)
+		{
+			return "byte[]";
+		}
+		if (pSqlTypeName.Equals("sql_variant"))
+		{
+			return "object";
+		}
+		return "Unknown";
+
 	}
 
 }
