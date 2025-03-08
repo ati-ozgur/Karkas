16fffed4cac2d807336e7452b42bbf9d66ccfe17
Atilla Ozgur <ati.ozgur@gmail.com>
2025-03-08 15:41:59 +0100

    new test is added

--- Karkas.Tests/HelperOracleDataTypesTest.cs
+++ Karkas.Tests/HelperOracleDataTypesTest.cs
@@ -6,6 +6,57 @@ namespace Karkas.Tests.CodeGeneration.Oracle.Tests;
 public class HelperOracleDataTypesTest
 {
 
+	// ANSI Supported Data Types
+	// https://docs.oracle.com/en/database/oracle/oracle-database/23/sqlqr/Data-Types.html
+
+	// Even though following data types are supported by Oracle Database in CREATE TABLE statements,
+	// They are STILL changed to internal oracle types.
+	// { CHARACTER[VARYING] (size)
+	// | { CHAR | NCHAR } VARYING(size)
+	// | VARCHAR(size)
+	// | NATIONAL { CHARACTER | CHAR }
+	//      [VARYING] (size)
+
+	// FOR example
+	// 	CREATE TABLE ANSI_STRING(
+	// 		C1 CHARACTER(5),
+	// 	C2 CHARACTER VARYING(5),
+	// 	C3 NATIONAL CHARACTER(5),
+	// 	C4 NATIONAL CHARACTER VARYING(5)
+	// );
+	// is changed to
+	// C1 CHAR(5)
+	// C2 VARCHAR2(5)
+	// C3 NCHAR(5)
+	// C4 NVARCHAR2(5)
+	// Thus we only need to test for char and varchar2, nchar and nvarchar2
+
+
+	[Theory]
+	[InlineData("CHAR", "string")]
+	[InlineData("VARCHAR2", "string")]
+	[InlineData("NCHAR", "string")]
+	[InlineData("NVARCHAR2", "string")]
+	public void ANSISupportCharacter_ShouldReturnString(string oracleType, string expectedDotNetType)
+	{
+		// Act
+		var result = HelperOracleDataTypes.GetDotNetType(oracleType);
+
+		// Assert
+		Assert.Equal(expectedDotNetType, result);
+	}
+
+	// | { NUMERIC | DECIMAL | DEC }
+	// [(precision[, scale])]
+	// | { INTEGER | INT | SMALLINT }
+	// | FLOAT[(size)]
+	// | DOUBLE PRECISION
+	// | REAL
+	// }
+
+
+
+
 	[Fact]
 	public void GetDotNetType_ShouldThrowException()
 	{
