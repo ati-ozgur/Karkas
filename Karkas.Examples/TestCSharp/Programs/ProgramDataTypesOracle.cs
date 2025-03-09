d27d14fc1aecfe88050fec20471cc188c7074d98
Atilla Ozgur <ati.ozgur@gmail.com>
2025-03-09 22:06:11 +0100

    change test code

--- Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
+++ Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
@@ -1,33 +1,25 @@
 ﻿
 ConnectionHelper.SetupDatabaseConnection();
 
-
-string maxInt64 = Int64.MaxValue.ToString();
-Console.WriteLine(maxInt64.Length);
-
-string maxInt128 = Int128.MaxValue.ToString();
-Console.WriteLine(maxInt128.Length);
-
-
 DecimalTablesBs bs1 = new DecimalTablesBs();
 
 int rowCount = 10;
 var resultList = bs1.QueryAll(rowCount);
-
-
 AssertEquals(resultList.Count,rowCount);
+Console.WriteLine("QueryAll maxRowCount works");
 
 foreach (var row in resultList)
 {
-	Console.WriteLine(row.DecimalTablesKey);
-	Console.WriteLine(row.DecimalTablesKey);
+    Console.WriteLine(row.DecimalTablesKey);
 }
 
 
-static void AssertEquals(object p1, object p2, string message = "should be equal")
+static void AssertEquals(int p1, int p2, string message = "should be equal")
 {
     if (p1 != p2)
     {
         throw new Exception($"Assertion error, {p1} != {p2} {message}");
     }
 }
+
+
