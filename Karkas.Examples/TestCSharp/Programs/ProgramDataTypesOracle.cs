f53633b28fb87b8fd81f15bf1904d0b7675c8e3d
Atilla Ozgur <ati.ozgur@gmail.com>
2025-04-30 19:18:09 +0200

    fix compile error

--- Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
+++ Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
@@ -5,7 +5,7 @@ DecimalTablesBs bs1 = new DecimalTablesBs();
 
 int rowCount = 10;
 var resultList = bs1.QueryAll(rowCount);
-AssertEquals(resultList.Count,rowCount);
+AssertEquals(resultList.Count,rowCount,"QueryAll rowCount works");
 Console.WriteLine("QueryAll maxRowCount works");
 
 foreach (var row in resultList)
@@ -14,7 +14,7 @@ foreach (var row in resultList)
 }
 
 
-static void assertEquals(object p1, object p2, string message)
+static void AssertEquals(object p1, object p2, string message)
 {
     if (!p1.Equals(p2))
     {
