0250531907eed374539fb23369b1150631a9b4e2
Atilla Ozgur <ati.ozgur@gmail.com>
2025-05-07 21:48:26 +0200

    new test

--- Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
+++ Karkas.Examples/TestCSharp/Programs/ProgramDataTypesOracle.cs
@@ -1,6 +1,33 @@
 ﻿
 ConnectionHelper.SetupDatabaseConnection();
 
+EmployeeBs bsEmployee = new EmployeeBs();
+
+Employee c1 = new Employee();
+c1.CustomerId = pk++;
+c1.FirstName = "Atilla";
+c1.LastName = "Özgür";
+c1.Email = "deneme@karkas.com.tr";
+
+bsEmployee.Insert(c1);
+
+var list = bsEmployee.QueryByFirstNameLastName("Atilla", "Özgür");
+
+if(list.Count > 0)
+{
+    Console.WriteLine("QueryByIndex generation works");
+}
+
+foreach (var c in list)
+{
+    Console.WriteLine(c.CustomerId + " " + c.FirstName + " " + c.LastName);
+}
+
+var c2 = bsEmployee.QueryByEmail(c1.Email);
+if(c1.FirstName == c2.FirstName)
+{
+    Console.WriteLine("QueryByIndex UNIQUE generation works");
+}
 
 Deneme d1 = new Deneme();
 d1.CInt = 1;
