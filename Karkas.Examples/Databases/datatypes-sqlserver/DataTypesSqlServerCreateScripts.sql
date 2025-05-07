41437505be78ca0b91ded250191ca9c9db22c422
Atilla Ozgur <ati.ozgur@gmail.com>
2025-05-07 22:18:20 +0200

    Customer table for Index examples

--- Karkas.Examples/Databases/datatypes-sqlserver/DataTypesSqlServerCreateScripts.sql
+++ Karkas.Examples/Databases/datatypes-sqlserver/DataTypesSqlServerCreateScripts.sql
@@ -9,4 +9,16 @@ CREATE TABLE Deneme (
       BitColumn  bit,
 );
 
+CREATE TABLE Customer
+(
+    CustomerId INT NOT NULL,
+    FirstName VARCHAR(40) NOT NULL,
+    LastName VARCHAR(20) NOT NULL,
+    Email NVARCHAR(60) NOT NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED (CustomerId)
+);
 
+
+CREATE INDEX Customer_FirstName_IDX ON DataTypes.dbo.Customer (FirstName, LastName);
+
+CREATE UNIQUE INDEX Customer_Email_IDX ON DataTypes.dbo.Customer (Email);
